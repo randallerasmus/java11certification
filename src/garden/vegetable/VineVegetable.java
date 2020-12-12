d69954992ef657729fbc4fe8e375c10dbc5036a4
randall.erasmus <randallerasmus1@gmail.com>
2020-12-12 04:14:30 +0200

    Initial commit

---
+++ src/garden/vegetable/VineVegetable.java
@@ -0,0 +1,37 @@
+package garden.vegetable;
+
+import jdk.jshell.ImportSnippet;
+
+public class VineVegetable {
+
+    public static void main(String[] args) {
+        System.out.println("im a vegetable that grows on a vine");
+    }
+
+//    PACKAGES AND IMPORTS
+
+/*    Referencing a type from another  packages
+*
+*     (1) - Fully Qaulified Class Name
+*
+*     For Example:
+*     java.util.ArrayList = new java.util.ArrayList();
+*
+*     (2) - Single Type import declaration
+*
+*     For Example:
+*     import java.util.ArrayList; --> this is the most common way
+*
+*     ArrayList aList = new ArrayList();
+*
+*     (3) Type import on demand declaration
+*
+*      import java.util.*;
+*      ArrayList aList = new ArrayList();
+*
+*      single static import on demand
+*       static import on demand
+*
+*
+* */
+}
